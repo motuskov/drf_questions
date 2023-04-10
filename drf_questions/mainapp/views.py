24080f7944a32040caee6678e057fa0d46cf06ee
Konstantin Motuskov <motuskov@gmail.com>
2023-04-10 13:36:10 -0300

    Add methods for setting the field "modified_by" of the Entity model

--- drf_questions/mainapp/views.py
+++ drf_questions/mainapp/views.py
@@ -15,6 +15,14 @@ class EntityViewSet(viewsets.ModelViewSet):
     queryset = Entity.objects.all()
     serializer_class = EntitySerializer
 
+    def perform_create(self, serializer):
+        serializer.validated_data['modified_by'] = self.request.user
+        return super().perform_create(serializer)
+
+    def perform_update(self, serializer):
+        serializer.validated_data['modified_by'] = self.request.user
+        return super().perform_update(serializer)
+
 class PropertyViewSet(viewsets.ModelViewSet):
     '''View set of Property class serializer
     '''
