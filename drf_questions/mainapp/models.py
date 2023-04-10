165a750ddec55c73756c2d852ac2e098f8a1f35c
Konstantin Motuskov <motuskov@gmail.com>
2023-04-10 12:17:36 -0300

    Add models

--- drf_questions/mainapp/models.py
+++ drf_questions/mainapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Property(models.Model):
+    '''Property class
+    '''
+    key = models.CharField(
+        max_length=100,
+        help_text='Key of the property'
+    )
+    value = models.CharField(
+        max_length=100,
+        help_text='Value of the property'
+    )
+
+class Entity(models.Model):
+    '''Entity class
+    '''
+    modified_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        help_text='User that modified the entity'
+    )
+    value = models.IntegerField(
+        help_text='Value of the entity'
+    )
+    properties = models.ManyToManyField(
+        Property,
+        help_text='Properties of the entity'
+    )
